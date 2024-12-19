fb19ffe364bfd4efcfdb58b993fe332857562004
Eugene <pshonovskij@gmail.com>
2024-12-19 23:31:19 +0200

    Update environment.prod.ts

--- src/ServerPulse.Frontend/src/environment/environment.prod.ts
+++ src/ServerPulse.Frontend/src/environment/environment.prod.ts
@@ -2,10 +2,11 @@ export const environment =
 {
   production: true,
   api: 'https://localhost:7129',
+  authApi: 'https://localhost:7129',
   maxAmountOfSlotsPerUser: 5,
   ecryptionSecretKey: "encryption-secret-key",
   projectUrl: "https://github.com/TEGTO/ServerPulse",
   statisticsHub: "https://localhost:7129/statisticshub",
   loadStatisticsHub: "https://localhost:7129/loadstatisticshub",
-  customStatisticsHub: "https://localhost:7129/customstatisticshub"
+  customStatisticsHub: "https://localhost:7129/customstatisticshub",
 };
